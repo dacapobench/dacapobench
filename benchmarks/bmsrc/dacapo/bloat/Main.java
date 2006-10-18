09fb71613b8eab8c532330a4751cf5aff02aaa2f
steveb-oss <devnull@localhost>
2006-10-18 10:05:57 +1000

    [svn r190] Woops.  Fixed a problem with those last additions.

--- benchmarks/bmsrc/dacapo/bloat/Main.java
+++ benchmarks/bmsrc/dacapo/bloat/Main.java
@@ -15,6 +15,6 @@ import dacapo.Benchmark;
 public class Main {
   public static void main(String args[]) throws Exception {
     // create dummy harness and invoke with dummy arguments
-	(new BloatHarness(null, null)).run(null, args, true);
+	(new BloatHarness(null, null)).run(null, "", true);
   }
 }
