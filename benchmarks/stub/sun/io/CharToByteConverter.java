92e91b596e84f774b5ccccd814a7a71dd0748921
steveb-oss <devnull@localhost>
2006-10-20 09:47:19 +1000

    [svn r204] Eric Bodden has filled out all methods for this class.  Thanks Eric.

--- benchmarks/stub/sun/io/CharToByteConverter.java
+++ benchmarks/stub/sun/io/CharToByteConverter.java
@@ -12,6 +12,7 @@ import java.io.UnsupportedEncodingException;
  * This class does the minimum required for the xalan benchmark to run.
  * 
  * @author Robin Garner
+ * @author Eric Bodden
  * @date $Date:$
  * @id $Id:$
  *
@@ -23,5 +24,44 @@ public class CharToByteConverter {
   }
   public boolean canConvert(char c) {
     return true;
+  }
+  public int getBadInputLength() {
+    return 256; //some made-up value
+  }
+  public int getMaxBytesPerChar() {
+    return 256; //some made-up value
+  }
+  public int nextByteIndex() {
+    return 256; //some made-up value
+  }
+  public int nextCharIndex() {
+    return 256; //some made-up value
+  }
+  public void reset() {}
+  public void setSubstitutionMode(boolean b) {}
+  public void setSubstitutionBytes(byte[] b) throws IllegalArgumentException {} 
+  public int flush(byte[] a,int b,int c) throws MalformedInputException, ConversionBufferFullException {
+    return 256; //some made-up value
+  }
+  public int flushAny(byte[] a,int b,int c) throws ConversionBufferFullException {
+    return 256; //some made-up value
+  }
+  public byte[] convertAll(char[] a) throws MalformedInputException {
+    return new byte[0];
+  }
+  public int convert(byte[] a,int b,int c) throws MalformedInputException, UnknownCharacterException, ConversionBufferFullException {
+    return 256; //some made-up value
+  }
+  public int convertAny(char[] a, int b, int c, byte[] d, int e, int f) throws ConversionBufferFullException {
+    return 256; //some made-up value
+  }
+  public String getCharacterEncoding() {
+    return "";
+  }
+  public String toString() {
+    return "";
+  }
+  public static CharToByteConverter getDefault() {
+    return instance;
   }
 }
