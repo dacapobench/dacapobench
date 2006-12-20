4d25673f7c9daf47ab8fbfda09804ffa4009dcf1
rgarner <devnull@localhost>
2006-12-20 18:03:43 +1100

    [svn r235] Just invokes org.mmtk.plan.Plan

--- benchmarks/src/MMTkHarness.java
+++ benchmarks/src/MMTkHarness.java
@@ -4,13 +4,9 @@
  */
 import java.lang.reflect.Method;
 
-class MMTkHarness {
+public class MMTkHarness {
   private static final boolean verbose = false;
-  private final String[] classes = new String[] {
-        "org.mmtk.plan.Plan",                                          // Post 2.4.6
-        "com.ibm.jikesrvm.memorymanagers.mminterface.MM_Interface",    // Post 2.4.5
-        "com.ibm.JikesRVM.memoryManagers.mmInterface.MM_Interface",    // Pre 2.4.5
-  };
+  private final String harnessClassName = "org.mmtk.plan.Plan";
     
   private Method beginMethod, endMethod;
     
@@ -18,32 +14,29 @@ class MMTkHarness {
    * Locate the class where the harness resides in various versions
    * of JikesRVM, and then retain Method references to the correct one.
    */
-  MMTkHarness() {
+  public MMTkHarness() {
     boolean found = false;
-    for (int i=0; i < classes.length && !found; i++) {
-      try {
-        if (verbose) System.out.println("Trying "+classes[i]);
-        Class harnessClass = Class.forName(classes[i]);
-        beginMethod = harnessClass.getMethod("harnessBegin", null);        // Just check for the method
-        endMethod = harnessClass.getMethod("harnessEnd", null);          // Just check for the method
-        found = true;
-      } catch (ClassNotFoundException c) {
-        if (verbose)
-          System.err.println("Could not locate "+classes[i]);
-      } catch (SecurityException e) {
-        if (verbose)
-          System.err.println("harness method of "+classes[i]+" is not accessible");
-      } catch (NoSuchMethodException e) {
-        if (verbose)
-          System.err.println("harness method of "+classes[i]+" not found");
-      }
+    try {
+      Class harnessClass = Class.forName(harnessClassName);
+      beginMethod = harnessClass.getMethod("harnessBegin", null);        // Just check for the method
+      endMethod = harnessClass.getMethod("harnessEnd", null);          // Just check for the method
+      found = true;
+    } catch (ClassNotFoundException c) {
+      if (verbose)
+        System.err.println("Could not locate "+harnessClassName);
+    } catch (SecurityException e) {
+      if (verbose)
+        System.err.println("harness method of "+harnessClassName+" is not accessible");
+    } catch (NoSuchMethodException e) {
+      if (verbose)
+        System.err.println("harness method of "+harnessClassName+" not found");
     }
     if (!found) {
       throw new RuntimeException("Unable to find MMTk Harness in any known parent class");
     }
   }
   
-  void harnessBegin() {
+  public void harnessBegin() {
     try {
       beginMethod.invoke(null, null);
     } catch (Exception e) {
@@ -51,7 +44,7 @@ class MMTkHarness {
     }
   }
   
-  void harnessEnd() {
+  public void harnessEnd() {
     try {
       endMethod.invoke(null, null);
     } catch (Exception e) {
