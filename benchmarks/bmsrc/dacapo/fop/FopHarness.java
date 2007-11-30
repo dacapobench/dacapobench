935b576315d0fb16e42b1e1c8beb0e1d59e02287
steveb-oss <devnull@localhost>
2007-11-30 15:39:44 +1100

    [svn r307] Updated fop to 0.94

--- benchmarks/bmsrc/dacapo/fop/FopHarness.java
+++ benchmarks/bmsrc/dacapo/fop/FopHarness.java
@@ -1,24 +1,31 @@
 package dacapo.fop;
 
 import java.io.File;
-import org.apache.fop.apps.Fop;
+import java.lang.reflect.Method;
 
 import dacapo.Benchmark;
 import dacapo.parser.Config;
 
 public class FopHarness extends Benchmark {
 
+  private Method method;
+
   public FopHarness(Config config, File scratch) throws Exception {
     super(config, scratch);
+    Class clazz = Class.forName("org.apache.fop.cli.Main", true, loader);
+    this.method = clazz.getMethod("startFOP", new Class[] { String[].class} );
   }
 
   public void iterate(String size) throws Exception {
     String[] args = preprocessArgs(size);
     /* Retarget input/output files into scratch directory */
-    for (int i=0; i < args.length; i++) 
+    for (int i=0; i < args.length; i++)
       if (args[i].charAt(0) != '-')
         args[i] = fileInScratch(args[i]);
-    Fop.main(args);
-  }
 
+    ClassLoader dacapoCL = Thread.currentThread().getContextClassLoader();
+    Thread.currentThread().setContextClassLoader(loader);
+    method.invoke(null, new Object[] {args});
+    Thread.currentThread().setContextClassLoader(dacapoCL);
+  }
 }
