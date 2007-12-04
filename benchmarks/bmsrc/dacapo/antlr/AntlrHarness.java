954cdecc4fe1ab2f13fb52c1dc34e65231729bac
steveb-oss <devnull@localhost>
2007-12-04 16:16:53 +1100

    [svn r314] Get antlr to use jar in scratch

--- benchmarks/bmsrc/dacapo/antlr/AntlrHarness.java
+++ benchmarks/bmsrc/dacapo/antlr/AntlrHarness.java
@@ -5,21 +5,22 @@ import java.util.Vector;
 
 import dacapo.Benchmark;
 import dacapo.parser.Config;
-import antlr.Tool;
 
 /**
  * Benchmark harness for the Antlr benchmark
  *
  * @author Robin Garner
  * @author Steve Blackburn
- * @date $Date: 2007-12-04 14:54:00 +1100 (Tue, 04 Dec 2007) $
- * @id $Id: AntlrHarness.java 310 2007-12-04 03:54:00Z steveb-oss $
+ * @date $Date: 2007-12-04 16:16:53 +1100 (Tue, 04 Dec 2007) $
+ * @id $Id: AntlrHarness.java 314 2007-12-04 05:16:53Z steveb-oss $
  *
  */
 public class AntlrHarness extends Benchmark {
 
   public AntlrHarness(Config config, File scratch) throws Exception {
     super(config,scratch);
+    Class<?> clazz = Class.forName("antlr.Tool", true, loader);
+    this.method = clazz.getMethod("main", new Class[] { String[].class} );
   }
 
   protected void prepare() throws Exception {
@@ -36,7 +37,7 @@ public class AntlrHarness extends Benchmark {
       deleteTree(new File(scratch,"antlr/output"));
   }
 
-  public void iterate(String size) {
+  public void iterate(String size) throws Exception {
     String[] args = preprocessArgs(size);
     int firstGrammarIndex = 0;
     int nIterations = 1;
@@ -57,6 +58,8 @@ public class AntlrHarness extends Benchmark {
       }
     }
 
+    ClassLoader dacapoCL = Thread.currentThread().getContextClassLoader();
+    Thread.currentThread().setContextClassLoader(loader);
     for (int iteration=0; iteration < nIterations; iteration++) {
       for (int iGrammar=firstGrammarIndex;
            iGrammar < args.length;
@@ -64,8 +67,10 @@ public class AntlrHarness extends Benchmark {
         String grammarFile = (new File(scratch,args[iGrammar])).getPath();
         newArgs[newArgs.length-1] = grammarFile;
         System.out.println(args[iGrammar]);
-        Tool.main(newArgs);
+
+        method.invoke(null, new Object[] {newArgs});
       }
     }
-  }
+    Thread.currentThread().setContextClassLoader(dacapoCL);
+ }
 }
