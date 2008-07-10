51d52dd9eaf899c8f7002e23b5b9dfe8ff91c0f5
steveb-oss <devnull@localhost>
2008-07-10 15:29:09 +1000

    [svn r357] Fix problem with last commit.  Bloat appears to work soundly now.

--- benchmarks/bmsrc/dacapo/bloat/BloatHarness.java
+++ benchmarks/bmsrc/dacapo/bloat/BloatHarness.java
@@ -1,20 +1,16 @@
 package dacapo.bloat;
 
 import java.io.File;
-import java.lang.reflect.Constructor;
 
 import dacapo.Benchmark;
 import dacapo.parser.Config;
 
 public class BloatHarness extends Benchmark {
 
-  private final Constructor<?> constructor;
-
   public BloatHarness(Config config, File scratch) throws Exception {
     super(config, scratch);
     Class<?> clazz = Class.forName("EDU.purdue.cs.bloat.optimize.Main", true, loader);
-    this.method = clazz.getMethod("main");
-    this.constructor = clazz.getConstructor(String[].class);
+    this.method = clazz.getMethod("main", String[].class);
    }
   
   public void prepare() throws Exception {
@@ -26,9 +22,6 @@ public class BloatHarness extends Benchmark {
   }
 
   public void iterate(String size) throws Exception {
-    String[] args = preprocessArgs(size);
-    Object object = constructor.newInstance((Object)args);
-    method.invoke(object);
-   }
-
+    method.invoke(null, (Object) preprocessArgs(size));
+  }
 }
