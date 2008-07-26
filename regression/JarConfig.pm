b5be314f5d33efbd1dea1eda4a6e5061fa78f102
steveb-oss <devnull@localhost>
2008-07-26 11:33:10 +1000

    [svn r383] update to reflect new build targets

--- regression/JarConfig.pm
+++ regression/JarConfig.pm
@@ -22,7 +22,7 @@ $dacapo_head_jar = "head";
 $dacapo_svn_dir = "dacapo-svn";
 $dacapo_clean = "rm -rf $dacapo_svn_dir $root_dir/$jar_path/dacapo-$dacapo_head_jar.jar";
 $dacapo_co = "svn co https://dacapobench.svn.sourceforge.net/svnroot/dacapobench/benchmarks/trunk $dacapo_svn_dir";
-$dacapo_build = "(cd $dacapo_svn_dir/benchmarks && $ant sources dist && mv dacapo-*.jar $root_dir/$jar_path/dacapo-$dacapo_head_jar.jar)";
+$dacapo_build = "(cd $dacapo_svn_dir/benchmarks && $ant dist && mv dacapo-*.jar $root_dir/$jar_path/dacapo-$dacapo_head_jar.jar)";
 
 @old_bm_list = ("antlr", "bloat", "chart", "eclipse", "fop", "hsqldb", "jython", "lusearch", "luindex", "pmd", "xalan");
 
