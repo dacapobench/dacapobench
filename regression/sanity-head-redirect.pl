8a2645c8b73299572660ce95fb5b4890b57f5007
jzigman <devnull@localhost>
2009-07-30 11:25:39 +1000

    [svn r504] Changed latest to be a link to the latest regression sanity results.

--- regression/sanity-head-redirect.pl
+++ regression/sanity-head-redirect.pl
@@ -5,8 +5,6 @@ use VMConfig;
 use JarConfig;
 use RunConfig;
 use strict;
-use XML::Writer;
-use Image::Magick;
 
 # strictly we only use the $id
 my ($id, $max_hour_id) = @ARGV;
@@ -16,39 +14,9 @@ my $log_rel_dir = "regression/log";
 my $log_dir = "$root_dir/$log_rel_dir";
 my $publish_html = "$root_dir/www/regression/sanity";
 
-produce_sanity_head_redirect_html($id);
+my $latest_link = "$publish_html/latest";
+my $latest_dir = "$publish_html/$id";
 
-# produce the index page which links to each vm's sanity results
-sub produce_sanity_head_redirect_html() {
-  my ($id) = @_;
-  my $name="latest.html";
-  my $output;
-  open $output, (">$publish_html/$name");
-  my $writer = XML::Writer->new(OUTPUT => $output);
-  my $ref = "$id/index.html";
-
-  # create header
-  $writer->startTag('html');
-  $writer->startTag('head');
-
-  # note that the sanity pages are expected to live in ../../sanity/$id/...
-  $writer->emptyTag('meta',
-      'http-equiv' => "REFRESH",
-      content => "0;url=$ref");
-
-  $writer->endTag('head');
-  $writer->startTag('body');
-
-  $writer->characters("This page should automatically redirect you to the sanity tests for the head current at ");
-  $writer->startTag('a',
-                    href => $ref);
-  $writer->characters($ref);
-  $writer->endTag('a');
-
-  
-  $writer->endTag('body');
-  $writer->endTag('html');
-
-  close $output;
-}
+system("rm -f $latest_link");
+system("ln -s $latest_dir $latest_link");
 
