68316f747042e8f9b87c3303dbe9e2485d402037
jzigman <devnull@localhost>
2009-09-10 15:59:23 +1000

    [svn r535] Changed the dacapo.jar cli for daytrader to be loaded relative to the supplied scratch directory. Removed reduntent main from daytrader Launcher.

--- benchmarks/libs/daytrader/src/org/dacapo/daytrader/Launcher.java
+++ benchmarks/libs/daytrader/src/org/dacapo/daytrader/Launcher.java
@@ -15,7 +15,7 @@ public class Launcher {
   private final static String SERVER_CLI_ENTRYPOINT = "org.apache.geronimo.cli.daemon.DaemonCLI";
   private final static String[] CLIENT_BIN_JARS = { "client.jar"};
   private final static String[] SERVER_BIN_JARS = { "server.jar"};
-  private final static String[] DACAPO_CLI_JAR = { "scratch/jar/daytrader.jar"};
+  private final static String[] DACAPO_CLI_JAR = { "jar/daytrader.jar"};
   // The following list is defined in the "Class-Path:" filed of MANIFEST.MF for the client and server jars
   private final static String[] GERONIMO_LIB_JARS = { "geronimo-cli-"+GVERSION+".jar", "geronimo-kernel-"+GVERSION+".jar", "geronimo-transformer-"+GVERSION+".jar", "asm-3.1.jar", "asm-commons-3.1.jar", "commons-cli-1.0.jar", "commons-logging-1.0.4.jar", "cglib-nodep-2.1_3.jar", "log4j-1.2.14.jar", "xpp3-1.1.3.4.0.jar", "xstream-1.2.2.jar"};
 
@@ -28,12 +28,6 @@ public class Launcher {
   private static File scratch = null;
 
   private static ServerThread serverThread;
-  
-  public static void main(String[] args) {
-    File scratch = new File((new File("./scratch")).getAbsolutePath());
-    initialize(scratch, 16, "small", true);
-    performIteration();
-  }
   
   public static void initialize(File scratchdir, int threads, String dtSize, boolean beans) {
     numThreads = threads;
@@ -80,7 +74,7 @@ public class Launcher {
 
       Thread.currentThread().setContextClassLoader(clientCLoader);
 
-      Class<?> clazz = Class.forName(CLIENT_CLI_ENTRYPOINT, true, clientCLoader);
+      Class<?> clazz = clientCLoader.loadClass(CLIENT_CLI_ENTRYPOINT);
       Method method = clazz.getMethod("main", new Class[] { String[].class});
       method.invoke(null, new Object[] {args});
     } catch (Exception e) {
@@ -98,7 +92,7 @@ public class Launcher {
       }
       Thread.currentThread().setContextClassLoader(serverCLoader);
       
-      Class<?> clazz = Class.forName(SERVER_CLI_ENTRYPOINT, true, serverCLoader);
+      Class<?> clazz = serverCLoader.loadClass(SERVER_CLI_ENTRYPOINT);
       Method method = clazz.getMethod("main", new Class[] { String[].class});
       method.invoke(null, new Object[] {args});
     } catch (Exception e) {
@@ -112,11 +106,10 @@ public class Launcher {
     System.setProperty("org.apache.geronimo.base.dir", geronimo.getPath());
     System.setProperty("java.ext.dirs", geronimo.getPath() + "/lib/ext:" +System.getProperty("java.home")+"/lib/ext");
     System.setProperty("java.io.tmpdir", geronimo.getPath() + "/var/temp");
-    
   }
   
   private static ClassLoader createGeronimoClassLoader(boolean server) throws Exception {
-    File geronimo = new File(scratch, GDIRECTORY);
+    File geronimo = new File(scratch, GDIRECTORY).getAbsoluteFile();
     ClassLoader oldCL = Thread.currentThread().getContextClassLoader();
     ClassLoader binCL = createClassLoader(oldCL, getGeronimoBinaryJar(geronimo, server), false);
     ClassLoader libCL = createClassLoader(binCL, getGeronimoLibraryJars(geronimo, server), true);
@@ -154,13 +147,11 @@ public class Launcher {
       File endorsed = new File(geronimo, "lib/endorsed");
       addJars(jars, endorsed, endorsed.list());
     } else {
-      File cwd = new File(".");
-      addJars(jars, cwd, DACAPO_CLI_JAR);
+      addJars(jars, scratch, DACAPO_CLI_JAR);
     }
    
     File lib = new File(geronimo, "lib");
     addJars(jars, lib, GERONIMO_LIB_JARS);
-    
     
     return (URL[]) jars.toArray(new URL[jars.size()]);
   }
