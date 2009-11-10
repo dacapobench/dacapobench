d46914a8b778afea138914b01dbcdd9239952306
jzigman <devnull@localhost>
2009-11-11 09:57:51 +1100

    [svn r607] Changed over to using apache commons cli for command line parsing.  Further clean up of the CommandLineArgs class is required.

--- benchmarks/harness/src/org/dacapo/harness/CommandLineArgs.java
+++ benchmarks/harness/src/org/dacapo/harness/CommandLineArgs.java
@@ -16,6 +16,15 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.jar.JarEntry;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+
 /**
  * Command line arguments for a dacapo benchmark run.
  * 
@@ -27,167 +36,180 @@ import java.util.jar.JarEntry;
  */
 public class CommandLineArgs {
   
-  private static final String RELEASE_NOTES = "RELEASE_NOTES.txt";
+  private final static int EXIT_OK                 = 0;
+  private final static int EXIT_MISSING_CALLBACK   = 2;
+  private final static int EXIT_BAD_CALLBACK       = 3;
+  private final static int EXIT_BAD_COMMANDLINE    = 4;
+  private final static int EXIT_MISSING_BENCHMARKS = 10;
   
+  private static final String RELEASE_NOTES             = "RELEASE_NOTES.txt";
+  private static final String DEFAULT_SIZE              = "default";
+  private static final String DEFAULT_SCRATCH_DIRECTORY = "./scratch";
+  private static final int    DEFAULT_MAX_ITERATIONS    = 20;
+  private static final int    DEFAULT_WINDOW_SIZE       = 3;
+  private static final double DEFAULT_VARIANCE          = 3.0/100;
+  private static final int    DEFAULT_ITERATIONS        = 1;
+  
+  private static final String OPT_CALLBACK          = "callback";
+  private static final String OPT_HELP              = "help";
+  private static final String OPT_RELEASE_NOTES     = "release-notes";
+  private static final String OPT_LIST_BENCHMARKS   = "list-benchmarks";
+  private static final String OPT_INFORMATION       = "information";
+  private static final String OPT_SIZE              = "size";
+  private static final String OPT_SCRATCH_DIRECTORY = "scratch-directory";
+  private static final String OPT_CONVERGE          = "converge";
+  private static final String OPT_MAX_ITERATIONS    = "max-iterations";
+  private static final String OPT_VARIANCE          = "variance";
+  private static final String OPT_WINDOW            = "window";
+  private static final String OPT_ITERATIONS        = "iterations";
+  private static final String OPT_DEBUG             = "debug";
+  private static final String OPT_IGNORE_VALIDATION = "ignore-validation";
+  private static final String OPT_NO_DIGEST_OUTPUT  = "no-digest-output";
+  private static final String OPT_NO_VALIDATION     = "no-validation";
+  private static final String OPT_PRESERVE          = "preserve";
+  private static final String OPT_VALIDATION_REPORT = "validation-report";
+  private static final String OPT_CONFIG            = "config";
+  private static final String OPT_VERBOSE           = "verbose";
+  
+  private static final Option[] OPTIONS = {
+    makeOption("c",  OPT_CALLBACK,          "Use class <callback> to bracket benchmark runs","callback"),
+    makeOption("h",  OPT_HELP,              "Print this help",null),
+    makeOption("r",  OPT_RELEASE_NOTES,     "Print the release notes",null),
+    makeOption("l",  OPT_LIST_BENCHMARKS,   "List available benchmarks",null),
+    makeOption("i",  OPT_INFORMATION,       "Display benchmark information",null),
+    makeOption("s",  OPT_SIZE,              "Size of input data","SIZE"),
+    makeOption(null, OPT_SCRATCH_DIRECTORY, "Specify an alternate scratch directory <dir>","dir"),
+    makeOption("C",  OPT_CONVERGE,          "Allow benchmark times to converge before timing",null),
+    makeOption(null, OPT_MAX_ITERATIONS,    "Run a max of <max_iterations> iterations (default 20)","max_iterations"),
+    makeOption(null, OPT_VARIANCE,          "Target coefficient of variation <pct> (default 3.0)","pct"),
+    makeOption(null, OPT_WINDOW,            "Measure variance over <window> runs (default 3)","window"),
+    makeOption("n",  OPT_ITERATIONS,        "Run the benchmark <iter> times","iter"),
+    makeOption("d",  OPT_DEBUG,             "Verbose debugging information",null),
+    makeOption(null, OPT_IGNORE_VALIDATION, "Don't halt on validation failure",null),
+    makeOption(null, OPT_NO_DIGEST_OUTPUT,  "Turn off SHA1 digest of stdout/stderr",null),
+    makeOption(null, OPT_NO_VALIDATION,     "Don't validate at all",null),
+    makeOption(null, OPT_PRESERVE,          "Preserve output files (debug)",null),
+    makeOption(null, OPT_VALIDATION_REPORT, "Report digests, line counts etc","report_file"),
+    makeOption(null, OPT_CONFIG,            null,"config_file"),
+    makeOption("v",  OPT_VERBOSE,           "Verbose output",null)
+  };
+  
+  private static CommandLineParser parser         = new PosixParser();
+  private static Options           options        = new Options();
+  private static Options           visibleOptions = new Options();
+
+  {
+    // Construct the option list and the visibleOption list.
+    // The option list is used for parsing the command line, 
+    // where as the visibleOption is a subset of the option list
+    // and is used for producing the usage help.
+    for(int i = 0; i<OPTIONS.length; i++) {
+      options.addOption(OPTIONS[i]);
+      if (OPTIONS[i].getDescription()!=null) visibleOptions.addOption(OPTIONS[i]);
+    }
+  }
+
   public enum Methodology { ITERATE, CONVERGE; }
 
-  private boolean verbose = false;    // 
-  private boolean debug = false;      // Enable debugging output
+  private CommandLine line;
+  
+  private boolean verbose = false; 
+  private boolean debug = false;
   private boolean silent = true;
   private boolean allowOpenFromFileSystem = false;
   private Methodology methodology = Methodology.ITERATE;
-  private double targetVar = 3.0/100;
-  private int window = 3;
-  private int maxIterations = 20;
+  private double targetVar = DEFAULT_VARIANCE;
+  private int window = DEFAULT_WINDOW_SIZE;
+  private int maxIterations = DEFAULT_MAX_ITERATIONS;
   private boolean ignoreValidation = false;
-  private int iterations = 1;
-  private String size = "default";
-  private String scratchDir = "./scratch";
+  private int iterations = DEFAULT_ITERATIONS;
+  private String size = DEFAULT_SIZE;
+  private String scratchDir = DEFAULT_SCRATCH_DIRECTORY;
   private String cnf = null;
   private Callback callback = null;
   private boolean info = false;
   
   private List<String> benchmarks = new ArrayList<String>();
 
-  
   CommandLineArgs(String[] args) throws Exception {
-    /* No options - print usage and die */
-    if (args.length == 0) {
-      printUsage();
-      return;
-    }
-    
-    /* get global options */
-    int i = 0;
-    for (; i < args.length && args[i].charAt(0) == '-'; i++) {
-      try {
-      if (args[i].equals("-s")) {
-        // size name name
-        if (i == args.length - 1) {
-          System.err.println("No size specified! (\"-h\" for usage)");
-          System.exit(10);          
-        }
-        this.size = args[++i];
-      } else if (args[i].equals("-i")) {
-        // display benchmark information
-        this.info = true;
-      } else if (args[i].equals("-h")) {
-        printUsage();
-        System.exit(0);
-      } else if (args[i].equals("-r")) {
-        printReleaseNotes();
-        System.exit(0);
-      } else if (args[i].equals("-l")) {
-        // list benchmarks
+    try {
+      boolean reportAndExitOk = false;
+      line = parser.parse(options, args);
+      
+      if (line.hasOption(OPT_SIZE)) this.size = line.getOptionValue(OPT_SIZE);
+      if (line.hasOption(OPT_INFORMATION)) this.info = true;
+      if (line.hasOption(OPT_LIST_BENCHMARKS)) {
         printBenchmarks();
-        System.exit(0);
-      } else if (args[i].equals("-v")) {
+        reportAndExitOk = true;
+      }
+      if (line.hasOption(OPT_RELEASE_NOTES)) {
+        printReleaseNotes();
+        reportAndExitOk = true;
+      }
+      if (line.hasOption(OPT_HELP)) {
+        printUsage();
+        reportAndExitOk = true;
+      }
+      if (reportAndExitOk) System.exit(EXIT_OK);
+      
+      if (line.hasOption(OPT_VERBOSE)) {
         // display detailed information
         this.verbose = true;
         this.silent = false;
-      } else if (args[i].equals("-c")) {
+      }
+      if (line.hasOption(OPT_CALLBACK)) {
         // use a callback
-        if (i == args.length - 1) {
-          System.err.println("No callback class specified! (\"-h\" for usage)");
-          System.exit(11);          
-        }
         Class<?> cls = null;
         try {
-          cls = Class.forName(args[++i]);
+          cls = Class.forName(line.getOptionValue(OPT_CALLBACK));
         } catch (Exception e) {
           System.err.println(e);
-          System.err.println("Could not find callback class "+args[i]);
-          System.exit(12);
+          System.err.println("Could not find callback class "+line.getOptionValue(OPT_CALLBACK));
+          System.exit(EXIT_MISSING_CALLBACK);
         } 
         if (!(Callback.class.isAssignableFrom(cls))) {
-          System.err.println(args[i] + " is not an instance of Callback");
-          System.exit(13);
+          System.err.println(line.getOptionValue(OPT_CALLBACK) + " is not an instance of Callback");
+          System.exit(EXIT_BAD_CALLBACK);
         } else {
           Constructor<?> cons = cls.getConstructor(new Class[] { CommandLineArgs.class });
           this.callback = ((Callback) cons.newInstance(new Object[] {this}));
         }
-      } else if (args[i].equals("-n")) {          // Run n times, showing the last iteration
-        if (i == args.length - 1) {
-          System.err.println("Number of iterations not specified! (\"-h\" for usage)");
-          System.exit(14);          
-        }
-        this.iterations = Integer.parseInt(args[++i]);
-      } else if (args[i].equals("-converge")) {   // Run until times converge
-        this.methodology = Methodology.CONVERGE;
-      } else if (args[i].equals("-cnf")) {
-        if (i == args.length - 1) {
-          System.err.println("No cnf file specified! (\"-h\" for usage)");
-          System.exit(21);    
-        }
-        this.cnf = args[++i];     
-      } else if (args[i].equals("-max_iterations")) { // Max iterations for convergence
-        if (i == args.length - 1) {
-          System.err.println("No max specified! (\"-h\" for usage)");
-          System.exit(15);          
-        }
-        this.maxIterations = Integer.parseInt(args[++i]);
-      } else if (args[i].equals("-variance")) {   // Coeff. of variance to aim for
-        if (i == args.length - 1) {
-          System.err.println("No variance specified! (\"-h\" for usage)");
-          System.exit(16);          
-        }
-        this.targetVar = Double.parseDouble(args[++i])/100.0;
-      } else if (args[i].equals("-window")) {     // # iterations to average convergence over
-        if (i == args.length - 1) {
-          System.err.println("No window size specified!  (\"-h\" for usage)");
-          System.exit(17);          
-        }
-        this.window = Integer.parseInt(args[++i]);
-      } else if (args[i].equals("-debug")) {      // Verbose benchmark output
-        Benchmark.setVerbose(true);
-        debug = true;
-      } else if (args[i].equals("-preserve")) {   // Preserve scratch directory contents
-        Benchmark.setPreserve(true);
-      } else if (args[i].equals("-noDigestOutput")) {  // 
-        Benchmark.setValidateOutput(false);
-      } else if (args[i].equals("-ignoreValidation")) {
-        this.ignoreValidation = true;
-      } else if (args[i].equals("-noValidation")) {
-        Benchmark.setValidate(false);
-      } else if (args[i].equals("-validationReport")) {
-    	  if (i == args.length - 1) {
-    		  System.err.println("No validation report filename specified! (\"-h\" for usage)");
-    		  System.exit(21);          
-    	  }
-        Benchmark.enableValidationReport(args[++i]);
-      } else if (args[i].equals("-scratch")) {
-        if (i == args.length - 1) {
-          System.err.println("No scratch directory specified! (\"-h\" for usage)");
-          System.exit(18);          
-        }
-        this.scratchDir = args[++i];
-      } else {
-        System.err.println("Unrecognized option "+args[i]+ " (\"-h\" for usage)");
-        System.exit(1);
       }
-      } catch (NumberFormatException e) {
-        System.err.println("Could not parse numeric argument to \""+args[i-1]+"\"! (\"-h\" for usage)");
-        System.exit(19);        
-      }
+      if (line.hasOption(OPT_ITERATIONS)) this.iterations = Integer.parseInt(line.getOptionValue(OPT_ITERATIONS));
+      if (line.hasOption(OPT_CONVERGE)) this.methodology = Methodology.CONVERGE;
+      if (line.hasOption(OPT_CONFIG)) this.cnf = line.getOptionValue(OPT_CONFIG);     
+      if (line.hasOption(OPT_MAX_ITERATIONS)) this.maxIterations = Integer.parseInt(line.getOptionValue(OPT_MAX_ITERATIONS));
+      if (line.hasOption(OPT_VARIANCE)) this.targetVar = Double.parseDouble(line.getOptionValue(OPT_VARIANCE))/100.0;
+      if (line.hasOption(OPT_WINDOW)) this.window = Integer.parseInt(line.getOptionValue(OPT_WINDOW));
+      if (line.hasOption(OPT_DEBUG)) this.debug = true;
+      if (line.hasOption(OPT_PRESERVE)) Benchmark.setPreserve(true);
+      if (line.hasOption(OPT_NO_DIGEST_OUTPUT)) Benchmark.setValidateOutput(false);
+      if (line.hasOption(OPT_IGNORE_VALIDATION)) this.ignoreValidation = true;
+      if (line.hasOption(OPT_NO_VALIDATION)) Benchmark.setValidate(false);
+      if (line.hasOption(OPT_VALIDATION_REPORT)) Benchmark.enableValidationReport(line.getOptionValue(OPT_VALIDATION_REPORT));
+      if (line.hasOption(OPT_SCRATCH_DIRECTORY)) this.scratchDir = line.getOptionValue(OPT_SCRATCH_DIRECTORY);
+
+      
+    } catch (ParseException e) {
+      System.err.println("Command line exception: " + e.getMessage());
+      System.exit(EXIT_BAD_COMMANDLINE);
+    } catch (Exception e) {
+      System.err.println("Exception processing command line values: " + e.getMessage());
+      System.exit(EXIT_BAD_COMMANDLINE);
     }
     
-    if (i == args.length) {
-        System.err.println("No benchmarks specified! (\"-h\" for usage)");
-        System.exit(20);        
+    // check that at least one benchmark is specified
+    if (line.getArgList().isEmpty()) {
+      printUsage();
+      System.exit(EXIT_MISSING_BENCHMARKS);
     }
     
     // now get the benchmark names
-    for (; i < args.length; i++) {
-      String bm = args[i];
-      if (bm.charAt(0) == '-') {
-        System.err.println(args[i]+ " is not a benchmark name (\"-h\" for usage)");
-        System.exit(1);
-      }
-      
-      benchmarks.add(bm);
+    for(Object bm: line.getArgList()) {
+      benchmarks.add((String)bm);
     }
     
-    /* Add the default callback */
+    // Add the default callback
     if (getCallback() == null) {
       callback = new Callback(this);
     }
@@ -197,56 +219,19 @@ public class CommandLineArgs {
    * Print a usage message to stdout
    */
   static void printUsage() {
-    System.out.println("Usage: java -jar dacapo-<version>.jar [options ...] [benchmarks ...]");
-    System.out.println("    -c <callback>           Use class <callback> to bracket benchmark runs");
-    System.out.println("    -h                      Print this help");
-    System.out.println("    -r                      Print the release notes");
-    System.out.println("    -l                      List available benchmarks");
-    System.out.println("    -i                      Display benchmark information");
-    System.out.println("    -s small|default|large  Size of input data");
-    System.out.println("    -scratch <dir>          Specify an alternate scratch directory");
-    System.out.println();
-    System.out.println("  Measurement methodology options");
-    System.out.println("    -converge               Allow benchmark times to converge before timing");
-    System.out.println("      -max_iterations <n>     Run a max of n iterations (default 20)");
-    System.out.println("      -variance <pct>         Target coefficient of variation (default 3.0)");
-    System.out.println("      -window <n>             Measure variance over n runs (default 3)");
-    System.out.println("    -n <iter>               Run the benchmark <iter> times");
-    System.out.println();
-    System.out.println("  Debugging options (for benchmark suite maintainers)");
-    System.out.println("    -debug                  Verbose debugging information");
-    System.out.println("    -ignoreValidation       Don't halt on validation failure");
-    System.out.println("    -noDigestOutput         Turn off SHA1 digest of stdout/stderr");
-    System.out.println("    -noValidation           Don't validate at all");
-    System.out.println("    -preserve               Preserve output files (debug)");
-    System.out.println("    -v                      Verbose output");
-    System.out.println("    -validationReport <file>  Report digests, line counts etc");
-    System.out.println();
-    System.out.println("  General usage");
-    System.out.println("    Please see the DaCapo web page (http://www.dacapobench.org) and");
-    System.out.println("    the release notes included in this jar for general advice on");
-    System.out.println("    using this suite (\"-r\" will print release notes).");
-    System.out.println();
-    System.out.println("    Please be sure to always quote the version number of the suite");
-    System.out.println("    when reporting results.  In accordance with the usual standards");
-    System.out.println("    of acknowledging credit in academic research, please cite the");
-    System.out.println("    suite appropriately (http://doi.acm.org/10.1145/1167473.1167488).");
-
-    }
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp("DaCapo Benchmark suite", visibleOptions);
+  }
   
   /**
    * Print the release notes to System.out
    */
-  static void printReleaseNotes() {
+  static void printReleaseNotes() throws IOException {
     BufferedReader releaseNotes = new BufferedReader(new InputStreamReader(CommandLineArgs.class.getClassLoader().getResourceAsStream(RELEASE_NOTES)));
-	
-    try {
-      String line;
-      while ((line = releaseNotes.readLine()) != null) {
-        System.out.println(line);
-      }
-    } catch (IOException e) {
-      System.err.println("Error reading release notes.");
+
+    String line;
+    while ((line = releaseNotes.readLine()) != null) {
+      System.out.println(line);
     }
   }
   
@@ -352,5 +337,27 @@ public class CommandLineArgs {
 
   public boolean isDebug() {
     return debug;
+  }
+  
+  /* Define a commandline option.
+   * @param shortName An optional short form name for the command line option.
+   * @param longname A commandline option must have a long form name.
+   * @param description All commandline options that are visible options must have a
+   *    description, commandline options that are for internal development usage must not
+   *    have a description and must instead be documented in the code.
+   * @param argName A commandline option that requires has an argument must specify an 
+   *    argument name.
+   */
+  private static Option makeOption(String shortName, String longName, String description, String argName) {
+    assert longName != null;
+    
+    Option option = new Option(shortName, longName, argName!=null, description); //(String opt, String longOpt, boolean hasArg, String description)
+    
+    if (argName!=null) {
+      option.setValueSeparator('=');
+      option.setArgName(argName);
+    }
+    
+    return option;
   }
 }
