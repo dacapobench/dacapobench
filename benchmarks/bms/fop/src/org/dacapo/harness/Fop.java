1473a9a7da8396d6acaf9226cdb8bdddbb4e4c6d
jzigman <devnull@localhost>
2009-12-03 11:51:16 +1100

    [svn r639] Standardize formatting with eclipse default, except spaces only and indentation of 2

--- benchmarks/bms/fop/src/org/dacapo/harness/Fop.java
+++ benchmarks/bms/fop/src/org/dacapo/harness/Fop.java
@@ -12,28 +12,27 @@ public class Fop extends Benchmark {
   public Fop(Config config, File scratch) throws Exception {
     super(config, scratch);
     Class<?> clazz = Class.forName("org.apache.fop.cli.Main", true, loader);
-    this.method = clazz.getMethod("startFOP", new Class[] { String[].class} );
+    this.method = clazz.getMethod("startFOP", new Class[] { String[].class });
   }
 
   @Override
   protected void prepare(String size) throws Exception {
     super.prepare(size);
-    args = config.preprocessArgs(size,scratch);
+    args = config.preprocessArgs(size, scratch);
     /* Retarget input/output files into scratch directory */
-    for (int i=0; i < args.length; i++)
+    for (int i = 0; i < args.length; i++)
       if (args[i].charAt(0) != '-')
         args[i] = fileInScratch(args[i]);
   }
 
   public void iterate(String size) throws Exception {
-    method.invoke(null, new Object[] {args});
+    method.invoke(null, new Object[] { args });
   }
-  
+
   /**
-   * Stub which exists <b>only</b> to facilitate whole program
-   * static analysis on a per-benchmark basis.  See also the "split-deps"
-   * ant build target, which is also provided to enable whole program
-   * static analysis.
+   * Stub which exists <b>only</b> to facilitate whole program static analysis
+   * on a per-benchmark basis. See also the "split-deps" ant build target, which
+   * is also provided to enable whole program static analysis.
    * 
    * @author Eric Bodden
    */
