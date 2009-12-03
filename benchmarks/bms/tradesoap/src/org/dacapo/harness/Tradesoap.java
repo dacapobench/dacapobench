5d6f35c90da7ef5fd4ffe3ce3c397d0a48ea1397
jzigman <devnull@localhost>
2009-12-03 13:24:20 +1100

    [svn r648] Standardize formatting with eclipse default, except spaces only and indentation of 2

--- benchmarks/bms/tradesoap/src/org/dacapo/harness/Tradesoap.java
+++ benchmarks/bms/tradesoap/src/org/dacapo/harness/Tradesoap.java
@@ -10,53 +10,55 @@ import org.dacapo.parser.Config;
  * Dacapo benchmark harness for tradesoap.
  * 
  * @author Apache
- *
+ * 
  */
 
 public class Tradesoap extends Benchmark {
   private Method initializeMethod;
-    
+
   public Tradesoap(Config config, File scratch) throws Exception {
-    super(config,scratch,false);
-    Class<?> clazz = Class.forName("org.dacapo.daytrader.Launcher", true, loader);
-    this.initializeMethod = clazz.getMethod("initialize", new Class[] { File.class, Integer.TYPE, String.class, Boolean.TYPE } );
-    this.method = clazz.getMethod("performIteration", new Class[] { } );
+    super(config, scratch, false);
+    Class<?> clazz = Class.forName("org.dacapo.daytrader.Launcher", true,
+        loader);
+    this.initializeMethod = clazz.getMethod("initialize", new Class[] {
+        File.class, Integer.TYPE, String.class, Boolean.TYPE });
+    this.method = clazz.getMethod("performIteration", new Class[] {});
   }
-  
+
   @Override
   protected void prepare() throws Exception {
     unpackZipFileResource("dat/daytrader.zip", scratch);
   }
-  
+
   @Override
   protected void prepare(String size) throws Exception {
-    String[] args = config.preprocessArgs(size,scratch);
+    String[] args = config.preprocessArgs(size, scratch);
     String dtSize = "medium";
     if (args.length == 1)
       dtSize = args[0];
-    
-    initializeMethod.invoke(null, scratch, config.getThreadCount(size), dtSize, false);
+
+    initializeMethod.invoke(null, scratch, config.getThreadCount(size), dtSize,
+        false);
   }
-  
+
   public void cleanup() {
     System.out.println("Shutting down Geronimo...");
     if (!getPreserve()) {
-      deleteTree(new File(scratch,"tradesoap"));
-      deleteTree(new File(scratch,"geronimo-jetty6-minimal-2.1.4"));
+      deleteTree(new File(scratch, "tradesoap"));
+      deleteTree(new File(scratch, "geronimo-jetty6-minimal-2.1.4"));
     }
   }
-  
+
   public void iterate(String size) throws Exception {
     if (getVerbose())
       System.out.println("tradesoap benchmark starting");
     method.invoke(null);
   }
-  
+
   /**
-   * Stub which exists <b>only</b> to facilitate whole program
-   * static analysis on a per-benchmark basis.  See also the "split-deps"
-   * ant build target, which is also provided to enable whole program
-   * static analysis.
+   * Stub which exists <b>only</b> to facilitate whole program static analysis
+   * on a per-benchmark basis. See also the "split-deps" ant build target, which
+   * is also provided to enable whole program static analysis.
    * 
    * @author Eric Bodden
    */
