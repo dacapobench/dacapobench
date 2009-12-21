40104caf3f9e90ae760b77116f16db95f787bda5
steveb-oss <devnull@localhost>
2009-12-21 14:33:48 +1100

    [svn r718] Fix sunflow so it actually respects thread count.  Fix copyright

--- benchmarks/bms/sunflow/src/org/dacapo/harness/Sunflow.java
+++ benchmarks/bms/sunflow/src/org/dacapo/harness/Sunflow.java
@@ -1,25 +1,11 @@
-/*
- * This class is directly derived from org.sunflow.Benchmark
- *  
- *  Copyright (c) 2003-2007 Christopher Kulla
- *  
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of
- *  this software and associated documentation files (the "Software"), to deal in the
- *  Software without restriction, including without limitation the rights to use, copy,
- *  modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
- *  and to permit persons to whom the Software is furnished to do so, subject to the
- *  following conditions:
- *  
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- *  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+/*******************************************************************************
+ * Copyright (c) 2009 The Australian National University.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0
+ *
+ * @date $Date:$
+ * @id $Id:$
+ *******************************************************************************/
 package org.dacapo.harness;
 
 import java.io.File;
@@ -41,8 +27,7 @@ public class Sunflow extends org.dacapo.harness.Benchmark {
     this.method = clazz.getMethod("kernelMain");
     this.beginMethod = clazz.getMethod("kernelBegin");
     this.endMethod = clazz.getMethod("kernelEnd");
-    this.constructor = clazz.getConstructor(int.class, boolean.class,
-        boolean.class, boolean.class);
+    this.constructor = clazz.getConstructor(int.class, boolean.class, boolean.class, boolean.class, int.class);
   }
 
   /** Do one-time prep such as unziping data. In our case, do nothing. */
@@ -61,8 +46,7 @@ public class Sunflow extends org.dacapo.harness.Benchmark {
     String[] args = config.preprocessArgs(size, scratch);
     useBenchmarkClassLoader();
     try {
-      sunflow = constructor.newInstance(Integer.parseInt(args[0]), false,
-          false, false);
+      sunflow = constructor.newInstance(Integer.parseInt(args[0]), false, false, false, config.getThreadCount(size));
       beginMethod.invoke(sunflow);
     } finally {
       revertClassLoader();
