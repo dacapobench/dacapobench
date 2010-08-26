8bcec28c8bbf5cc17574f07b3d50814a67deda9d
john <john@coda>
2010-08-26 13:05:00 +1000

    added the property dacapo.callback.classpath which can be used to specify the path for finding user defined Callback classes

--- benchmarks/harness/src/Harness.java
+++ benchmarks/harness/src/Harness.java
@@ -8,39 +8,69 @@
  * @date $Date: 2009-12-24 11:19:36 +1100 (Thu, 24 Dec 2009) $
  * @id $Id: Harness.java 738 2009-12-24 00:19:36Z steveb-oss $
  */
+import java.io.File;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-
 public class Harness {
- 
+
   // The DaCapo Harness is in a separate library within the dacapo.jar
   // allowing a separate class loader to be used for loading the dacapo harness
-  private static final String  HARNESS_PATH   = "harness/";
-  private static final String  HARNESS_CLASS  = "org.dacapo.harness.TestHarness";
-  private static final String  HARNESS_METHOD = "main";
-	
-  public static void main(String[] args) throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-	ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-	
-	URL harnessJarURL = classLoader.getResource(HARNESS_PATH);
-	
-	ClassLoader harnessClassLoader = new URLClassLoader(new URL[] { harnessJarURL });
+  private static final String HARNESS_PATH = "harness/";
+  private static final String HARNESS_CLASS = "org.dacapo.harness.TestHarness";
+  private static final String HARNESS_METHOD = "main";
 
-	Thread.currentThread().setContextClassLoader(harnessClassLoader);
-	
-	Class dacapoHarnessClass = harnessClassLoader.loadClass(HARNESS_CLASS);
+  //
+  private static final String CALLBACK_CLASSPATH_PROPERTY = "dacapo.callback.classpath";
 
-	Method harnessMain = dacapoHarnessClass.getDeclaredMethod(HARNESS_METHOD, String[].class);
-	
-	harnessMain.invoke(null, new Object[] { args });
+  public static void main(String[] args) throws MalformedURLException,
+      URISyntaxException, ClassNotFoundException, SecurityException,
+      NoSuchMethodException, IllegalArgumentException, IllegalAccessException,
+      InvocationTargetException {
+    ClassLoader harnessClassLoader = makeHarnessClassLoader();
 
-	/* TestHarness.main(args); */
+    Thread.currentThread().setContextClassLoader(harnessClassLoader);
+
+    Class dacapoHarnessClass = harnessClassLoader.loadClass(HARNESS_CLASS);
+
+    Method harnessMain = dacapoHarnessClass.getDeclaredMethod(HARNESS_METHOD, String[].class);
+
+    harnessMain.invoke(null, new Object[] { args });
+
     System.exit(0);
   }
-  
-  
+
+  private static ClassLoader makeHarnessClassLoader() throws MalformedURLException, URISyntaxException {
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    String dacapoCallbackClasspath = System
+        .getProperty(CALLBACK_CLASSPATH_PROPERTY);
+
+    URL harnessJarURL = classLoader.getResource(HARNESS_PATH);
+
+    URL[] urls = null;
+
+    if (dacapoCallbackClasspath != null) {
+      File file = new File(dacapoCallbackClasspath);
+      URI callbackClasspath = null;
+        
+      if (file.isDirectory())
+        callbackClasspath = file.getAbsoluteFile().toURI();
+      else
+        callbackClasspath = new URI(dacapoCallbackClasspath);
+
+      if (callbackClasspath == null)
+        throw new URISyntaxException(dacapoCallbackClasspath, "is not a URI nor a directory nor a class file");
+      
+      urls = new URL[] { harnessJarURL, callbackClasspath.toURL() };
+    } else
+      urls = new URL[] { harnessJarURL };
+
+    return new URLClassLoader(urls);
+  }
 }
