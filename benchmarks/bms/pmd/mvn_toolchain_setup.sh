da6879cc18852766d40a510804a87c1810c58c5b
John Zhang <john.jiabin.zhang@gmail.com>
2017-12-06 17:36:11 +1100

    WIP: PMD 5.7.0 now compiles
    PMD 5.7.0 use Maven as its build system. The `build.xml` for the
    benchmark has been changed to call Maven.
    Also, it requires Java JDK toolchain to be configured for Maven. A
    shell script has been added to generate the toolchain.xml file.
    
    Note that during the building process, PMD requires both 1.7 and 1.8
    toolchain to be configured. A clean solution would require the JDK7 to
    be installed on the platfor along with JDK8/9. This requirement can be
    annoying. Therefore the generated toolchain.xml file
    has a hack such that if the platform java version is >= 1.8, it will
    generate two entries (1.7 and the actual version) that point to the same
    JDK directory.
    
    Also the download link for the release has been changed to GitHub.

---
+++ benchmarks/bms/pmd/mvn_toolchain_setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# This script sets up the $HOME/.m2/toolchains.xml if it does not already exist
+
+extract_java_version() {
+    java -version |& grep "java version" | sed '{ s/java version "[0-9]\.\([0-9]\+\).*"/\1/ }'
+}
+
+if [ ! -e $1 ]; then
+    jver=`extract_java_version`
+    if [ $jver -ge 8 ]; then
+        cat > $1 <<- EOM
+<?xml version="1.0" encoding="UTF8"?>
+<toolchains>
+  <toolchain>
+    <type>jdk</type>
+    <provides>
+      <version>1.7</version>    <!-- # though the actual version might be greater, pmd requires 1.7 here. -->
+    </provides>
+    <configuration> 
+      <jdkHome>$JAVA_HOME</jdkHome>
+    </configuration>
+  </toolchain>
+  <toolchain>
+    <type>jdk</type>
+    <provides>
+      <version>$jver</version>
+    </provides>
+    <configuration> 
+      <jdkHome>$JAVA_HOME</jdkHome>
+    </configuration>
+  </toolchain>
+</toolchains>
+EOM
+  else
+    echo "PMD benchmark needs Java >= 1.8!" >> /dev/stderr
+    exit 1
+  fi
+fi
