d2445466ecd83b3c0ddd19eacd9ec193dc0c51f0
Rui Chen <u5958754@anu.edu.au>
2017-12-11 15:17:50 +1100

    Modifing the some codes for newer version tomcat and also updating the checksum for all stuffs

--- benchmarks/bms/tomcat/src/org/dacapo/tomcat/StartApp.java
+++ benchmarks/bms/tomcat/src/org/dacapo/tomcat/StartApp.java
@@ -20,6 +20,6 @@ public class StartApp extends HttpGet {
    * @param path The path to the webapp to start
    */
   public StartApp(String path) {
-    super("/manager/start?path="+path,200,null);
+    super("/manager/text/start?path="+path,200,null);
   }
 }
