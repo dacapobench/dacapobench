b2e5176c28ec21250dbf6b80ae213a568446d5fb
John Zhang <john.zhang@anu.edu.au>
2018-09-11 12:13:21 +1000

    added graphchi-java as a new benchmark

---
+++ benchmarks/bms/graphchi/harness/src/org/dacapo/harness/GraphChi.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2009 The Australian National University.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0.
+ * You may obtain the license at
+ * 
+ *    http://www.opensource.org/licenses/apache2.0.php
+ */
+package org.dacapo.harness;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.dacapo.harness.Benchmark;
+import org.dacapo.parser.Config;
+
+public class GraphChi extends Benchmark {
+
+  private String[] args;
+  private Class<?> cls;
+
+  public GraphChi(Config config, File scratch) throws Exception {
+    super(config, scratch, false);
+  }
+
+  @Override
+  protected void prepare(String size) throws Exception {
+    super.prepare(size);
+
+    String[] config_args = config.preprocessArgs(size, scratch);
+    String strAppClsName = config_args[0];
+    args = Arrays.copyOfRange(config_args, 1, config_args.length);
+    
+    cls = Class.forName("edu.cmu.graphchi.apps." + strAppClsName, true, loader);
+    this.method = cls.getMethod("main", new Class[] { String[].class });
+  }
+
+  public void iterate(String size) throws Exception {
+    method.invoke(null, (Object)args);
+  }
+}
