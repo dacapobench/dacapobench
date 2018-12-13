390c032d7eeb8233adcaf053c385b91d1265dcec
Rui Chen <rui.chen@anu.edu.au>
2018-12-13 19:12:32 +0800

    [BioJava] Added a new fasta file(large). Added patch file for fixing stdout problem and building problem

--- benchmarks/bms/biojava/harness/src/org/dacapo/harness/BioJava.java
+++ benchmarks/bms/biojava/harness/src/org/dacapo/harness/BioJava.java
@@ -10,7 +10,7 @@ public class BioJava extends Benchmark {
     String[] args;
 
     public BioJava(Config config, File scratch, File data) throws Exception {
-        super(config, scratch, data, false);
+        super(config, scratch, data);
         Class<?> clazz = Class.forName("org.biojava.nbio.aaproperties.CommandPrompt", true, loader);
         this.method = clazz.getMethod("main", String[].class);
     }
@@ -23,9 +23,6 @@ public class BioJava extends Benchmark {
 
     @Override
     public void iterate(String size) throws Exception {
-        System.out.println(System.out.toString());
         this.method.invoke(null, (Object) new String[]{"-i", args[0], "-a"});
-        System.out.println(System.out.toString());
-        System.out.println("in here I am out");
     }
 }
