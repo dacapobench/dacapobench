ef9f0e591e29e838b4b2cf71d404b993641205d7
Rui Chen <rui.chen@anu.edu.au>
2019-06-03 16:02:00 +1000

    [Daytrader] update the new daytrader dacapo files

---
+++ benchmarks/libs/daytrader/daytrader/javaee6/modules/web/src/main/java/org/apache/geronimo/daytrader/javaee6/dacapo/DaCapoClient.java
@@ -0,0 +1,49 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.geronimo.daytrader.javaee6.dacapo;
+
+public class DaCapoClient {
+	int completed;
+
+	public static void main(String[] args) {
+		boolean initialize = false;
+		boolean beans = false;
+		int numThreads = 64;
+		String size = "tiny";
+		try {
+			for (int i = 0; i < args.length; i++) {
+				if ("-t".equals(args[i])) {
+					numThreads = Integer.parseInt(args[++i]);
+				} else if ("-i".equals(args[i])) {
+					initialize = true;
+				} else if ("-s".equals(args[i])) {
+					size = args[++i];
+				} else if ("-b".equals(args[i])) {
+					beans = true;
+				}
+			}
+			if (initialize) {
+				DaCapoTrader.initializeTrade(size);
+			} else {
+				DaCapoRunner.runDaCapoTrade(size, numThreads, !beans);
+			}
+		} catch (Exception e) {
+			System.err.println("Caught an unexpected exception!");
+			e.printStackTrace();
+		}
+	}
+}
