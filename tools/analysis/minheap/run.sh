d468545523177b00b6016e521a56870947999e33
Steve Blackburn <steveblackburn@google.com>
2023-08-09 11:33:20 +0000

    Update zxing version

--- tools/analysis/minheap/run.sh
+++ tools/analysis/minheap/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 for i in 0 1 2; do
-  running minheap -a 3 minheap.yml minheap-out-10-$i.yml
+  running minheap -a 3 minheap-1.yml minheap-out-1-$i.yml
+  running minheap -a 3 minheap-10.yml minheap-out-10-$i.yml
 done
