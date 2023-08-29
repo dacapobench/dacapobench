8a254da2a47033e2c70b16059c3e4dbe35935a7a
Steve Blackburn <steveblackburn@google.com>
2023-08-29 09:01:17 +0000

    Update analysis scripts

--- tools/analysis/minheap/extract.sh
+++ tools/analysis/minheap/extract.sh
@@ -3,10 +3,18 @@
 # Etract min heap results and dump them in yml files
 #
 
-LINES=24 # number of benchmarks plus header (needed to parse output)
-BASE=`pwd`/../../..
+LINES=5 # number of benchmarks + 1 (used to parse file)
+BASE=`pwd`/../../../..
 OUT=stats-minheap.yml
-CFGS=("open-jdk-11.s.cp.gc-G1.t-32.f-10.n-1" "open-jdk-11.s.cp.gc-G1.t-1.f-10.n-1" "open-jdk-11.s.up.gc-G1.t-32.f-10.n-1" "open-jdk-11.s.cp.gc-Parallel.t-32.f-10.n-1" "open-jdk-17.s.cp.gc-G1.t-32.f-10.n-1" "open-jdk-11.s.cp.gc-G1.t-32.f-10.n-10")
+CFGS=("open-jdk-17.s.cp.gc-G1.t-32.f-10.n-1"\
+	  "open-jdk-17.sz-small.s.cp.gc-G1.t-1.f-10.n-1"\
+	  "open-jdk-17.sz-large.s.cp.gc-G1.t-32.f-10.n-1"\
+	  "open-jdk-17.sz-vlarge.s.cp.gc-G1.t-32.f-10.n-1"\
+	  "open-jdk-17.s.cp.gc-Parallel.t-32.f-10.n-1"\
+	  "open-jdk-17.s.cp.gc-G1.t-1.f-10.n-1"\
+	  "open-jdk-17.s.up.gc-G1.t-32.f-10.n-1"\
+	  "open-jdk-17.s.cp.gc-G1.t-32.f-10.n-10"\
+	  "open-jdk-11.s.cp.gc-G1.t-32.f-10.n-1")
 
 printf -v HEADER '#
 # Minimum heap sizes in which benchmark will run to completion, using
@@ -17,7 +25,16 @@ printf -v HEADER '#
 BMS=$BASE/benchmarks/bms
 
 for bm in `cd $BMS; ls`; do
-    CMDS=("java-11-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-78bcea0f.jar $bm -t 32 -f 10 -n 1" "java-11-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-78bcea0f.jar $bm -t 1 -f 10 -n 1" "java-11-openjdk-amd64 -server -XX:-UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-78bcea0f.jar $bm -t 32 -f 10 -n 1" "java-11-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseParallelGC -jar dacapo-evaluation-git-78bcea0f.jar $bm -t 32 -f 10 -n 1" "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-78bcea0f.jar $bm -t 32 -f 10 -n 1" "java-11-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-0d047f55.jar $bm -t 32 -f 10 -n 10")
+    CMDS=("java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 1"\
+          "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -s small -t 32 -f 10 -n 1"\
+          "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -s large -t 32 -f 10 -n 1"\
+          "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -s vlarge -t 32 -f 10 -n 1"\
+	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 1 -f 10 -n 1"\
+	  "java-17-openjdk-amd64 -server -XX:-UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 1"\
+	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseParallelGC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 1"\
+	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 1"\
+	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 10"\
+	  "java-11-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 1")
     if [ "$bm" != "common.xml" ]; then
 	echo $bm
 	out=$BMS/$bm/$OUT
@@ -25,17 +42,23 @@ for bm in `cd $BMS; ls`; do
 	for i in ${!CFGS[@]}; do
 	    cfg=${CFGS[$i]}
 	    line="$cfg: [ "
-            bc=$(echo $cfg | cut -d'.' -f1-6)
-	    n=$(echo $cfg | cut -d'.' -f7 | cut -c3-10)
+	    sz=$(echo $cfg | cut -d'.' -f2 | cut -c1-2)
+	    if [ "$sz" == "sz" ]; then # extra "sz" component
+		bc=$(echo $cfg | cut -d'.' -f1-7)
+		n=$(echo $cfg | cut -d'.' -f8 | cut -c3-10)
+	    else
+		bc=$(echo $cfg | cut -d'.' -f1-6)
+		n=$(echo $cfg | cut -d'.' -f7 | cut -c3-10)
+	    fi
 	    for j in 0 1 2; do
-		v=$(grep -A24 $bc ./minheap-out-$n-$j.yml | grep "$bm:" | cut -d' ' -f6 )
+		v=$(grep -A$LINES $bc ./minheap-out-$n-$j.yml | grep "$bm:" | cut -d' ' -f6 )
 		line="$line$v"
 		if [ "$j" != "2" ]; then
 		    line="$line, "
 		else
 		    line="$line ]"
 		    len=${#line}
-		    add=$( expr 64 - $len)
+		    add=$( expr 70 - $len)
 		    for k in `seq 1 $add`; do
 			line="$line ";
 		    done
---
+++ tools/analysis/perf/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+running runbms -i 10 -s 10,5,2,3,4,8,6,1,7,9 log runbms.yml
+running runbms -i 10 -s 2 log-extra runbms-extra.yml
