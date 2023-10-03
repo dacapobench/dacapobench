8b00562de33c49a4e832d42e877bcc1c80557ecc
Steve Blackburn <steveblackburn@google.com>
2023-10-03 03:50:52 +0000

    Updated min heap stats

--- tools/analysis/minheap/extract.sh
+++ tools/analysis/minheap/extract.sh
@@ -3,18 +3,19 @@
 # Etract min heap results and dump them in yml files
 #
 
-LINES=5 # number of benchmarks + 1 (used to parse file)
+LINES=23 # number of benchmarks + 1 (used to parse file)
 BASE=`pwd`/../../../..
+GH="68f38f5d"
 OUT=stats-minheap.yml
-CFGS=("open-jdk-17.s.cp.gc-G1.t-32.f-10.n-1"\
-	  "open-jdk-17.sz-small.s.cp.gc-G1.t-1.f-10.n-1"\
-	  "open-jdk-17.sz-large.s.cp.gc-G1.t-32.f-10.n-1"\
-	  "open-jdk-17.sz-vlarge.s.cp.gc-G1.t-32.f-10.n-1"\
-	  "open-jdk-17.s.cp.gc-Parallel.t-32.f-10.n-1"\
-	  "open-jdk-17.s.cp.gc-G1.t-1.f-10.n-1"\
-	  "open-jdk-17.s.up.gc-G1.t-32.f-10.n-1"\
-	  "open-jdk-17.s.cp.gc-G1.t-32.f-10.n-10"\
-	  "open-jdk-11.s.cp.gc-G1.t-32.f-10.n-1")
+CFGS=("open-jdk-17.ee.s.cp.gc-G1.t-32.f-10.n-1"\
+	  "open-jdk-17.sz-small.ee.s.cp.gc-G1.t-1.f-10.n-1"\
+	  "open-jdk-17.sz-large.ee.s.cp.gc-G1.t-32.f-10.n-1"\
+	  "open-jdk-17.sz-vlarge.ee.s.cp.gc-G1.t-32.f-10.n-1"\
+	  "open-jdk-17.ee.s.cp.gc-Parallel.t-32.f-10.n-1"\
+	  "open-jdk-17.ee.s.cp.gc-G1.t-1.f-10.n-1"\
+	  "open-jdk-17.ee.s.up.gc-G1.t-32.f-10.n-1"\
+	  "open-jdk-17.ee.s.cp.gc-G1.t-32.f-10.n-10"\
+	  "open-jdk-11.ee.s.cp.gc-G1.t-32.f-10.n-1")
 
 printf -v HEADER '#
 # Minimum heap sizes in which benchmark will run to completion, using
@@ -25,16 +26,15 @@ printf -v HEADER '#
 BMS=$BASE/benchmarks/bms
 
 for bm in `cd $BMS; ls`; do
-    CMDS=("java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 1"\
-          "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -s small -t 32 -f 10 -n 1"\
-          "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -s large -t 32 -f 10 -n 1"\
-          "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -s vlarge -t 32 -f 10 -n 1"\
-	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 1 -f 10 -n 1"\
-	  "java-17-openjdk-amd64 -server -XX:-UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 1"\
-	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseParallelGC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 1"\
-	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 1"\
-	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 10"\
-	  "java-11-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-744ef415.jar $bm -t 32 -f 10 -n 1")
+    CMDS=("java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-$GH.jar $bm -t 32 -f 10 -n 1"\
+          "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-$GH.jar $bm -s small -t 32 -f 10 -n 1"\
+          "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-$GH.jar $bm -s large -t 32 -f 10 -n 1"\
+          "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-$GH.jar $bm -s vlarge -t 32 -f 10 -n 1"\
+	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseParallelGC -jar dacapo-evaluation-git-$GH.jar $bm -t 32 -f 10 -n 1"\
+	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-$GH.jar $bm -t 1 -f 10 -n 1"\
+	  "java-17-openjdk-amd64 -server -XX:-UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-$GH.jar $bm -t 32 -f 10 -n 1"\
+	  "java-17-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-$GH.jar $bm -t 32 -f 10 -n 10"\
+	  "java-11-openjdk-amd64 -server -XX:+UseCompressedOops -XX:+ExitOnOutOfMemoryError -XX:+UseG1GC -jar dacapo-evaluation-git-$GH.jar $bm -t 32 -f 10 -n 1")
     if [ "$bm" != "common.xml" ]; then
 	echo $bm
 	out=$BMS/$bm/$OUT
@@ -44,11 +44,11 @@ for bm in `cd $BMS; ls`; do
 	    line="$cfg: [ "
 	    sz=$(echo $cfg | cut -d'.' -f2 | cut -c1-2)
 	    if [ "$sz" == "sz" ]; then # extra "sz" component
+		bc=$(echo $cfg | cut -d'.' -f1-8)
+		n=$(echo $cfg | cut -d'.' -f9 | cut -c3-10)
+	    else
 		bc=$(echo $cfg | cut -d'.' -f1-7)
 		n=$(echo $cfg | cut -d'.' -f8 | cut -c3-10)
-	    else
-		bc=$(echo $cfg | cut -d'.' -f1-6)
-		n=$(echo $cfg | cut -d'.' -f7 | cut -c3-10)
 	    fi
 	    for j in 0 1 2; do
 		v=$(grep -A$LINES $bc ./minheap-out-$n-$j.yml | grep "$bm:" | cut -d' ' -f6 )
