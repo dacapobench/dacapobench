7758c9357fe369e0924527fa8f8435ff1786ec7b
Steve Blackburn <steveblackburn@google.com>
2024-02-14 10:17:59 +0000

    Minor cleanup of bytecode analysis

--- tools/analysis/bcc/src/main/java/org/dacapo/analysis/BCCAnalysis.java
+++ tools/analysis/bcc/src/main/java/org/dacapo/analysis/BCCAnalysis.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-
+import java.io.File;
+import java.io.PrintStream;
 
 public class BCCAnalysis {
+
+    private static final String YML_FILENAME_PROPERTY = "dacapo.bccyml";
+
+    private static int iteration = 0;
+    private static String ymlSuffix = "";
 
     public static long classesTransformed= 0;
     public static Long bytecodesTransformed = new Long(0);
@@ -20,7 +26,7 @@ public class BCCAnalysis {
     public static long[] opcodes = new long[255];
 
 
-    public static String[] mnemonic = {"nop", "aconst_null", "iconst_m1", "iconst_0", "iconst_1", "iconst_2", "iconst_3", "iconst_4", "iconst_5", "lconst_0", "lconst_1", "fconst_0", "fconst_1", "fconst_2", "dconst_0", "dconst_1", "bipush", "sipush", "ldc", "ldc_w", "ldc2_w", "iload", "lload", "fload", "dload", "aload", "iload_0", "iload_1", "iload_2", "iload_3", "lload_0", "lload_1", "lload_2", "lload_3", "fload_0", "fload_1", "fload_2", "fload_3", "dload_0", "dload_1", "dload_2", "dload_3", "aload_0", "aload_1", "aload_2", "aload_3", "iaload", "laload", "faload", "daload", "aaload", "baload", "caload", "saload", "istore", "lstore", "fstore", "dstore", "astore", "istore_0", "istore_1", "istore_2", "istore_3", "lstore_0", "lstore_1", "lstore_2", "lstore_3", "fstore_0", "fstore_1", "fstore_2", "fstore_3", "dstore_0", "dstore_1", "dstore_2", "dstore_3", "astore_0", "astore_1", "astore_2", "astore_3", "iastore", "lastore", "fastore", "dastore", "aastore", "bastore", "castore", "sastore", "pop", "pop2", "dup", "dup_x1", "dup_x2", "dup2", "dup2_x1", "dup2_x2", "swap", "iadd", "ladd", "fadd", "dadd", "isub", "lsub", "fsub", "dsub", "imul", "lmul", "fmul", "dmul", "idiv", "ldiv", "fdiv", "ddiv", "irem", "lrem", "frem", "drem", "ineg", "lneg", "fneg", "dneg", "ishl", "lshl", "ishr", "lshr", "iushr", "lushr", "iand", "land", "ior", "lor", "ixor", "lxor", "iinc", "i2l", "i2f", "i2d", "l2i", "l2f", "l2d", "f2i", "f2l", "f2d", "d2i", "d2l", "d2f", "i2b", "i2c", "i2s", "lcmp", "fcmpl", "fcmpg", "dcmpl", "dcmpg", "ifeq", "ifne", "iflt", "ifge", "ifgt", "ifle", "if_icmpeq", "if_icmpne", "if_icmplt", "if_icmpge", "if_icmpgt", "if_icmple", "if_acmpeq", "if_acmpne", "goto", "jsr", "ret", "tableswitch", "lookupswitch", "ireturn", "lreturn", "freturn", "dreturn", "areturn", "return", "getstatic", "putstatic", "getfield", "putfield", "invokevirtual", "invokespecial", "invokestatic", "invokeinterface", "invokedynamic", "new", "newarray", "anewarray", "arraylength", "athrow", "checkcast", "instanceof", "monitorenter", "monitorexit", "wide", "multianewarray", "ifnull", "ifnonnull", "goto_w", "jsr_w", "breakpoint", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "impdep1", "impdep2"};
+    public static final String[] mnemonic = {"nop", "aconst_null", "iconst_m1", "iconst_0", "iconst_1", "iconst_2", "iconst_3", "iconst_4", "iconst_5", "lconst_0", "lconst_1", "fconst_0", "fconst_1", "fconst_2", "dconst_0", "dconst_1", "bipush", "sipush", "ldc", "ldc_w", "ldc2_w", "iload", "lload", "fload", "dload", "aload", "iload_0", "iload_1", "iload_2", "iload_3", "lload_0", "lload_1", "lload_2", "lload_3", "fload_0", "fload_1", "fload_2", "fload_3", "dload_0", "dload_1", "dload_2", "dload_3", "aload_0", "aload_1", "aload_2", "aload_3", "iaload", "laload", "faload", "daload", "aaload", "baload", "caload", "saload", "istore", "lstore", "fstore", "dstore", "astore", "istore_0", "istore_1", "istore_2", "istore_3", "lstore_0", "lstore_1", "lstore_2", "lstore_3", "fstore_0", "fstore_1", "fstore_2", "fstore_3", "dstore_0", "dstore_1", "dstore_2", "dstore_3", "astore_0", "astore_1", "astore_2", "astore_3", "iastore", "lastore", "fastore", "dastore", "aastore", "bastore", "castore", "sastore", "pop", "pop2", "dup", "dup_x1", "dup_x2", "dup2", "dup2_x1", "dup2_x2", "swap", "iadd", "ladd", "fadd", "dadd", "isub", "lsub", "fsub", "dsub", "imul", "lmul", "fmul", "dmul", "idiv", "ldiv", "fdiv", "ddiv", "irem", "lrem", "frem", "drem", "ineg", "lneg", "fneg", "dneg", "ishl", "lshl", "ishr", "lshr", "iushr", "lushr", "iand", "land", "ior", "lor", "ixor", "lxor", "iinc", "i2l", "i2f", "i2d", "l2i", "l2f", "l2d", "f2i", "f2l", "f2d", "d2i", "d2l", "d2f", "i2b", "i2c", "i2s", "lcmp", "fcmpl", "fcmpg", "dcmpl", "dcmpg", "ifeq", "ifne", "iflt", "ifge", "ifgt", "ifle", "if_icmpeq", "if_icmpne", "if_icmplt", "if_icmpge", "if_icmpgt", "if_icmple", "if_acmpeq", "if_acmpne", "goto", "jsr", "ret", "tableswitch", "lookupswitch", "ireturn", "lreturn", "freturn", "dreturn", "areturn", "return", "getstatic", "putstatic", "getfield", "putfield", "invokevirtual", "invokespecial", "invokestatic", "invokeinterface", "invokedynamic", "new", "newarray", "anewarray", "arraylength", "athrow", "checkcast", "instanceof", "monitorenter", "monitorexit", "wide", "multianewarray", "ifnull", "ifnonnull", "goto_w", "jsr_w", "breakpoint", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "impdep1", "impdep2"};
 
     /* Benchmark Callback hooks */
     public static void iterationStart(boolean warmup) {
@@ -28,6 +34,7 @@ public class BCCAnalysis {
     }
 
     public static void iterationStop(boolean warmup) {
+        ymlSuffix = warmup ? "."+iteration : "";
     }
 
     private static boolean check() {
@@ -50,6 +57,20 @@ public class BCCAnalysis {
         synchronized (bytecodesTransformed) {
             check();
 
+            PrintStream yml = System.out;
+
+            String ymlfile = System.getProperty(YML_FILENAME_PROPERTY);
+            if (ymlfile == null) {
+                System.out.println("The '"+YML_FILENAME_PROPERTY+"' system property is not set, so printing bytecode yml to console.");
+            } else {
+                ymlfile += ymlSuffix;
+                try {
+                    yml = new PrintStream(new File(ymlfile));
+                } catch (Exception e) {
+                    System.err.println("Could not open '"+ymlfile+"', so printing bytecode yml to console.");
+                }
+            }
+
             List<Long> bytecodefreq = new ArrayList<Long>(executed.values());
             Collections.sort(bytecodefreq);
             int uniq = bytecodefreq.size();
@@ -58,29 +79,38 @@ public class BCCAnalysis {
             long p999 = bytecodefreq.get((uniq-1)-(uniq/1000));
             long p9999 = bytecodefreq.get((uniq-1)-(uniq/10000));
 
-            System.out.println("transformed-classes: "+ classesTransformed);
-            System.out.println("transformed-bytecodes: "+ bytecodesTransformed);
-            System.out.println("executed-bytecodes: "+ bytecodesExecuted);
-            System.out.println("executed-bytecodes-unique: "+ executed.size());
-            System.out.println("executed-bytecodes-p90: "+ p90);
-            System.out.println("executed-bytecodes-p99: "+ p99);
-            System.out.println("executed-bytecodes-p999: "+ p999);
-            System.out.println("executed-bytecodes-p9999: "+ p9999);
-            System.out.println("executed-calls: "+ callsExecuted);
-            System.out.println("executed-calls-unique: "+ called.size());
+            yml.println("skipped-classes: " + skipped.size());
+            yml.println("transformed-classes: "+ classesTransformed);
+            yml.println("transformed-bytecodes: "+ bytecodesTransformed);
+            yml.println("executed-bytecodes: "+ bytecodesExecuted);
+            yml.println("executed-bytecodes-unique: "+ executed.size());
+            yml.println("executed-bytecodes-p90: "+ p90);
+            yml.println("executed-bytecodes-p99: "+ p99);
+            yml.println("executed-bytecodes-p999: "+ p999);
+            yml.println("executed-bytecodes-p9999: "+ p9999);
+            yml.println("executed-calls: "+ callsExecuted);
+            yml.println("executed-calls-unique: "+ called.size());
 
-            System.out.print("opcodes: {");
+            yml.print("opcodes: {");
             boolean start = true;
             for (int i = 0; i < 255; i++) {
                 if (opcodes[i] != 0) {
-                    System.out.print("\n  "+mnemonic[i]+": "+opcodes[i]);
+                    yml.print("\n  "+mnemonic[i]+": "+opcodes[i]);
                     start = false;
                 }
             }
-            System.out.println("\n}");
+            yml.println("\n}");
 
             for (String msg : skipped) {
-                System.out.println(msg);
+                yml.println("# "+msg);
+            }
+
+            if (yml != System.out) {
+                try {
+                    yml.close();
+                } catch (Exception e) {
+                    System.err.println("Exception closing file: "+e);
+                }
             }
         }
     }
@@ -99,7 +129,7 @@ public class BCCAnalysis {
 
     static void preMainClassLoaded(Class clazz) {
     }
-  
+
     static void classTransformed(String className) {
         classesTransformed++;
     }
@@ -111,7 +141,7 @@ public class BCCAnalysis {
         if (bytecodesTransformed >= Integer.MAX_VALUE) {
             System.err.println("WARNING: overflow on bytecode ID");
             return Integer.MAX_VALUE;
-        } else 
+        } else
             return bytecodesTransformed.intValue();
     }
 
