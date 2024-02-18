1def95eceda4f1f0bfe0395af8353ee44924f3e3
Steve Blackburn <steveblackburn@google.com>
2024-02-18 07:34:15 +0000

    WIP with class loading and transformation

--- tools/analysis/bcc/src/main/java/org/dacapo/analysis/Transformer.java
+++ tools/analysis/bcc/src/main/java/org/dacapo/analysis/Transformer.java
@@ -9,14 +9,14 @@ public class Transformer implements ClassFileTransformer {
     boolean booted = false;
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
 
-        for (ClassLoader curLoader = loader; ; curLoader = curLoader.getParent()) {
-            if (curLoader == null) {
-                String suffix = loader != null ? "for non-null loader " + loader : "with null loader";
-                BCCAnalysis.classSkipped("Skipped '" + className + "' " + suffix);
-                return null;
-             } else if (curLoader == BCCAnalysis.class.getClassLoader())
-                 break;
-        }
+        // for (ClassLoader curLoader = loader; ; curLoader = curLoader.getParent()) {
+        //     if (curLoader == null) {
+        //         String suffix = loader != null ? "for non-null loader " + loader : "with null loader";
+        //         BCCAnalysis.classSkipped("Skipped '" + className + "' " + suffix);
+        //         return null;
+        //      } else if (curLoader == BCCAnalysis.class.getClassLoader())
+        //          break;
+        // }
         BCCAnalysis.classTransformed(className);
         return new Rewriter(className, classfileBuffer).rewrite();
     }
