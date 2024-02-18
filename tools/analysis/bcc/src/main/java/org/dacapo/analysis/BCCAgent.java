46ae2459ad0d5a4b53820f90b6a386b2f1ef44bd
Steve Blackburn <steveblackburn@google.com>
2024-02-18 07:34:15 +0000

    WIP with class loading and transformation

--- tools/analysis/bcc/src/main/java/org/dacapo/analysis/BCCAgent.java
+++ tools/analysis/bcc/src/main/java/org/dacapo/analysis/BCCAgent.java
@@ -3,9 +3,20 @@ package org.dacapo.analysis;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
+import java.net.URI;
+import java.io.File;
+import java.util.jar.JarFile;
 
 public class BCCAgent {
     public static void premain(String agentArgs, Instrumentation inst) {
+        try {
+            URI uri = BCCAgent.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+            JarFile jar = new JarFile(new File(uri));
+//            inst.appendToBootstrapClassLoaderSearch(jar);
+        } catch (Exception e) {
+            System.err.println("Could not open jar. "+e);
+        }
+
         final ClassFileTransformer transformer = new Transformer();
 
 //        The following is not needed as we are not interested classes that have been loaded
