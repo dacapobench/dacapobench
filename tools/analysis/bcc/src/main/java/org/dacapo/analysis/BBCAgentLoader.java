bf334876842ecaccd914e767ccd15d6eddc68b38
Steve Blackburn <steveblackburn@google.com>
2024-02-19 05:00:39 +0000

    WIP dynamic loader

---
+++ tools/analysis/bcc/src/main/java/org/dacapo/analysis/BBCAgentLoader.java
@@ -0,0 +1,21 @@
+package org.dacapo.analysis;
+
+
+public class BCCAgentLoader {
+
+    private static final String jarFilePath = "/usr/local/google/home/steveblackburn/devel/dacapobench/benchmarks/"
+    public static void loadAgent() {
+        logger.info("dynamically loading javaagent");
+        String nameOfRunningVM = ManagementFactory.getRuntimeMXBean().getName();
+        int p = nameOfRunningVM.indexOf('@');
+        String pid = nameOfRunningVM.substring(0, p);
+
+        try {
+            VirtualMachine vm = VirtualMachine.attach(pid);
+            vm.loadAgent(jarFilePath, "");
+            vm.detach();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
