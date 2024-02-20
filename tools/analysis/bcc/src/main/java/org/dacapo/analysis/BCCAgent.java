aadc55514a433be5480f8bc7a9ef67e271ee8c1d
Steve Blackburn <steveblackburn@google.com>
2024-02-20 10:07:36 +0000

    Clean up bytecode analysis.  Eclipse still not working

--- tools/analysis/bcc/src/main/java/org/dacapo/analysis/BCCAgent.java
+++ tools/analysis/bcc/src/main/java/org/dacapo/analysis/BCCAgent.java
@@ -7,8 +7,55 @@ import java.net.URI;
 import java.io.File;
 import java.util.jar.JarFile;
 
+import org.dacapo.analysis.BytecodeCallback;
+
+
 public class BCCAgent {
+
+    private static volatile boolean canRewriteBootstrap;
+
+    static boolean canRewriteClass(String className, ClassLoader loader) {
+        // There are two conditions under which we don't rewrite:
+        //  1. If className was loaded by the bootstrap class loader and
+        //  the agent wasn't (in which case the class being rewritten
+        //  won't be able to call agent methods).
+        //  2. If it is java.lang.ThreadLocal, which can't be rewritten because the
+        //  JVM depends on its structure.
+        if (((loader == null) && !canRewriteBootstrap)
+            || className.startsWith("java/lang/ThreadLocal")) {
+          return false;
+        }
+        // third_party/java/webwork/*/ognl.jar contains bad class files.  Ugh.
+        if (className.startsWith("ognl/")) {
+          return false;
+        }
+
+        if (className.startsWith("org/dacapo/analysis")) {
+          return false;
+        }
+
+        return true;
+      }
+
     public static void premain(String agentArgs, Instrumentation inst) {
+        // Force eager class loading here.  The instrumenter relies on these classes.  If we load them
+        // for the first time during instrumentation, the instrumenter will try to rewrite them.  But
+        // the instrumenter needs these classes to run, so it will try to load them during that rewrite
+        // pass.  This results in a ClassCircularityError.
+        try {
+            Class.forName("sun.security.provider.PolicyFile");
+            Class.forName("java.util.ResourceBundle");
+            Class.forName("java.util.Date");
+        } catch (Throwable t) {
+            // NOP
+        }
+
+        if (!inst.isRetransformClassesSupported()) {
+            System.err.println("Some JDK classes are already loaded and will not be instrumented.");
+        }
+
+
+
 //        The following is not needed as we are not interested classes that have been loaded
 //        before this agent
 //        Class[] classes = inst.getAllLoadedClasses();
@@ -25,10 +72,4 @@ public class BCCAgent {
         final ClassFileTransformer transformer = new Transformer();
         inst.addTransformer(transformer, true);
     }
-
-    public static void agentmain(String agentArgs, Instrumentation inst) {
-        final ClassFileTransformer transformer = new Transformer();
-        inst.addTransformer(transformer, true);
-    }
-
 }
--- tools/analysis/bcc/src/main/java/org/dacapo/analysis/Transformer.java
+++ tools/analysis/bcc/src/main/java/org/dacapo/analysis/Transformer.java
@@ -2,13 +2,21 @@ package org.dacapo.analysis;
 
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.UnmodifiableClassException;
 import java.security.ProtectionDomain;
+
+import org.dacapo.analysis.BytecodeCallback;
 
 public class Transformer implements ClassFileTransformer {
 
     boolean booted = false;
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-
+        // if (classBeingRedefined == null)
+        //     return null;
+        if (!BCCAgent.canRewriteClass(className, loader)) {
+            BytecodeCallback.classSkipped(className);
+            throw new RuntimeException(new UnmodifiableClassException("cannot instrument " + className));
+        }
         // for (ClassLoader curLoader = loader; ; curLoader = curLoader.getParent()) {
         //     if (curLoader == null) {
         //         String suffix = loader != null ? "for non-null loader " + loader : "with null loader";
@@ -17,7 +25,7 @@ public class Transformer implements ClassFileTransformer {
         //      } else if (curLoader == BCCAnalysis.class.getClassLoader())
         //          break;
         // }
-        BCCAnalysis.classTransformed(className);
+        BytecodeCallback.classTransformed(className);
         return new Rewriter(className, classfileBuffer).rewrite();
     }
 }
