fd766f7ea90be8f9b5211bd56dc8e91588117558
Steve Blackburn <steve.blackburn@anu.edu.au>
2024-08-27 09:40:57 +0000

    Updated perf scripting

--- tools/analysis/perf/run.sh
+++ tools/analysis/perf/run.sh
@@ -1,4 +1,33 @@
 #!/usr/bin/bash
+# 
+# This script is for running on the default hardware configuration (Zen4)
+# 
+# The following jobs need to be run independently, additionally, on different hardware
+#
+# on ARM
+#  baseline_arm.yml
+#  singlecore_arm.yml
+# on Intel
+#  baseline_intel.yml
+#  singlecore_intel.yml
+# on AMD with turbo-bost changes
+#  boost.yml
+# on AMD with memory speed changes
+#  memory.yml
 
-running runbms -i 10 -s 1,10,5,2,3,4,8,6,7,9 log runbms.yml
-running runbms -i 10 -s 2 log-extra runbms-extra.yml
+mkdir -p ./data
+running runbms ./data ./baseline.yml -s 10,5,1,7,3,9,8,6,4,2 -p "dacapo-baseline"
+running runbms ./data ./compiler.yml -s 2 -p "dacapo-compiler"
+running runbms ./data ./gclog.yml -s 2 -p "dacapo-gclog" -i 1
+running runbms ./data ./llc.yml -s 2 -p "dacapo-llc"
+running runbms ./data ./tma.yml -s 2 -p "dacapo-tma"
+running runbms ./data ./tma_be.yml -s 2 -p "dacapo-pldi-2024-tma-be"
+running runbms ./data ./variants.yml -s 2 -p "dacapo-variants"
+running runbms ./data ./baseline_perf.yml -s 10,5,1,7,3,9,8,6,4,2 -p "dacapo-baseline"
+running runbms ./data ./variants_perf.yml -s 2 -p "dacapo-variants"
+
+
+
+# intel
+# arm
+# boost
