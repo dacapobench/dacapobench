29cd132c424332542402fcfd107c0aeae1f6e574
Steve Blackburn <steve.blackburn@anu.edu.au>
2024-10-13 06:13:12 +0000

    Update alloc stats

--- tools/analysis/run-alloc.sh
+++ tools/analysis/run-alloc.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
-VERSION=evaluation-git-6b469af0
+VERSION=dacapo-evaluation-git-3745e0c2
 BASEDIR=$(cd ../../benchmarks; pwd)
-JAVA=/usr/lib/jvm/temurin-21-jdk-amd64/bin/java # > 11
-JAR=$BASEDIR/dacapo-$VERSION.jar
+JAVA=/usr/lib/jvm/temurin-21-jdk-amd64/bin/java
+JAR=$BASEDIR/$VERSION.jar
 
-for bm in `$JAVA -jar $BASEDIR/dacapo-$VERSION.jar -l 2>/dev/null`; do
-    $JAVA -Ddacapo.alloc.yml=$BASEDIR/bms/$bm/stats-alloc.yml -Djava.security.manager=allow -javaagent:$BASEDIR/dacapo-$VERSION/jar/java-allocation-instrumenter-3.3.4.jar -jar $BASEDIR/dacapo-$VERSION.jar -callback org.dacapo.analysis.AllocCallback $bm
+for bm in `$JAVA -jar $JAR -l 2>/dev/null`; do
+    YML=$BASEDIR/bms/$bm/stats-alloc.yml
+    $JAVA -Ddacapo.alloc.yml=$YML -Dsys.ai.h2o.debug.allowJavaVersions=21 -Djava.security.manager=allow -javaagent:$BASEDIR/$VERSION/jar/java-allocation-instrumenter-3.3.4.jar -jar $JAR -callback org.dacapo.analysis.AllocCallback $bm
+    sed -i 's/\sThese\sstatistics/\n# Allocation statistics generated using version '$VERSION'\n#\n# These statistics/'g $YML
 done
